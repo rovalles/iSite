78b85e63b9551956440551bd2051815b16410f07
Rodolfo Ovalles <ro3014@att.com>
2015-12-29 01:43:52 -0600

    default site setup

--- generators/app/index.js
+++ generators/app/index.js
@@ -23,7 +23,7 @@ module.exports = yeoman.generators.Base.extend({
       type: 'string',
       name: 'projectName',
       message: 'What would like to title this application?',
-      default: 'angular app'
+      default: 'App Name'
     }];
 
     this.prompt(prompts, function (answers) {
@@ -37,35 +37,9 @@ module.exports = yeoman.generators.Base.extend({
     this.destinationRoot(this.appName);
   },
   writing: function () {
-    this.directory('', '');
+    this.directory( this.sourceRoot(), this.destinationRoot());
   },
   install: function () {
-    /* This has an issue I'll deal with it later.
-
-      var done = this.async();
-
-      this.log('\n\nInitializing Git repository. If this fail, try running ' +
-        chalk.yellow.bold('git init') +
-       ' and make a first commit manually');
-
-
-      async.series([
-        function (taskDone) {
-          exec('git config http.proxy http://one.proxy.att.com:8080', taskDone);
-        }
-      ], function (err) {
-
-        if (err === 127) {
-          this.log('Could not find the ' + chalk.yellow.bold('git') + ' command. Make sure Git is installed on this machine');
-          return;
-        }else{
-          this.log(chalk.green('complete') + ' Git repository has been setup');
-        }
-
-        done();
-      }.bind(this));
-    */
-
     this.installDependencies();
   },
   end: function () {
---
+++ generators/app/templates/index.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<!--[if lt IE 7]>  <html class="ie ie6 lte9 lte8 lte7"> <![endif]-->
+<!--[if IE 7]>     <html class="ie ie7 lte9 lte8 lte7"> <![endif]-->
+<!--[if IE 8]>     <html class="ie ie8 lte9 lte8"> <![endif]-->
+<!--[if IE 9]>     <html class="ie ie9 lte9"> <![endif]-->
+<!--[if gt IE 9]>  <html> <![endif]-->
+<!--[if !IE]><!--> 
+<html lang="en"><!--<![endif]-->
+	<head>
+		<meta charset="UTF-8">
+        <title><%= projectName %></title>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+		<!--link rel="stylesheet" href="bower_components/material-design-lite/material.min.css"-->
+		<link rel="stylesheet" href="assets/styles/main.css">
+		<!--[if lt IE 9]>
+				<script src="html5shiv.js"></script>
+		<![endif]-->
+	</head>
+	<body>
+	</body>
+	<script src="bower_components/material-design-lite/material.min.js"></script>
+	<script src="bower_components/jquery/dist/jquery.min.js"></script>
+	<script src="bower_components/mustache.js/mustache.min.js"></script>
+	<script src="assets/scripts/main.js"></script>
+</html>
